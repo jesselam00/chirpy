66c09a33ac6b6af6ae490fd2a6598148994f8660
Jesse Lam <jesselam00@Mac.home.local>
2025-10-31 15:08:13 -0500

    Add PUT request to users to update email/password when logged in with an active access token

--- main.go
+++ main.go
@@ -57,6 +57,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 	
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 
